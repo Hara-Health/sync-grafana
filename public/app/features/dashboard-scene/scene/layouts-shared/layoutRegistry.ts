57a7d6178d2c877f89cd7c16b42f8cf0dde1b25c
Liuri Loami <liuri@usesync.ai>
2024-11-20 16:18:50 -0300

    First version

--- public/app/features/dashboard-scene/scene/layouts-shared/layoutRegistry.ts
+++
@@ -1,9 +0,0 @@
-import { Registry } from '@grafana/data';
-
-import { DefaultGridLayoutManager } from '../layout-default/DefaultGridLayoutManager';
-import { ResponsiveGridLayoutManager } from '../layout-responsive-grid/ResponsiveGridLayoutManager';
-import { LayoutRegistryItem } from '../types';
-
-export const layoutRegistry: Registry<LayoutRegistryItem> = new Registry<LayoutRegistryItem>(() => {
-  return [DefaultGridLayoutManager.getDescriptor(), ResponsiveGridLayoutManager.getDescriptor()];
-});
